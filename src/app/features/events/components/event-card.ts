d32b9118323b0b926b7e76880942d2ef64dd3859
Christophe Barreto <dev-cba@pm.me>
2026-07-23 15:17:00 +0200

    refactor(architecture): split layout and features

--- src/app/features/events/components/event-card.ts
+++ src/app/features/events/components/event-card.ts
@@ -1,12 +1,11 @@
 import { Component, input } from "@angular/core";
-import { Event } from "../../models/event";
+import { EventModel } from "../models/event";
 
 @Component({
     selector: "app-event-card",
     imports: [],
     templateUrl: "./event-card.html",
-    styleUrl: "./event-card.scss",
 })
 export class EventCard {
-    event = input.required<Event>();
+    event = input.required<EventModel>();
 }
--- src/app/features/events/models/event.ts
+++ src/app/features/events/models/event.ts
@@ -1,4 +1,4 @@
-export interface Event {
+export interface EventModel {
     id: string;
     title: string;
     url: string;

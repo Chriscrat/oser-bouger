7be0552f6a1f58609380e178725265cd8b7051a0
Christophe Barreto <dev-cba@pm.me>
2026-07-23 14:56:14 +0200

    chore: add commitlint scope validation

---
+++ commitlint.config.mjs
@@ -0,0 +1,20 @@
+export default {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "scope-enum": [
+            2,
+            "always",
+            [
+                "app",
+                "layout",
+                "home",
+                "architecture",
+                "ui",
+                "event-list",
+                "events",
+                "card",
+                "map"
+            ],
+        ],
+    },
+};

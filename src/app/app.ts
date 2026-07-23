47f3f871982d726a41912e1e7cfc07078b45a09a
Christophe Barreto <dev-cba@pm.me>
2026-07-23 15:58:09 +0200

    feat(app): add routing and extract home page from app shell

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,12 @@
-import { Component, signal } from "@angular/core";
+import { Component } from "@angular/core";
 import { Header } from "./layout/components/header/header";
 import { Footer } from "./layout/components/footer/footer";
-import { Hero } from "./features/home/components/hero/hero";
-import { Alert } from "./ui/alert/components/alert";
-import { EventList } from "./features/events/components/event-list/event-list";
+import { RouterOutlet } from "@angular/router";
+
 @Component({
     selector: "app-root",
-    imports: [EventList, Header, Footer, Hero, Alert],
+    imports: [Header, Footer, RouterOutlet],
     templateUrl: "./app.html",
     styleUrl: "./app.scss",
 })
-export class App {
-    protected readonly title = signal("oser-bouger");
-    alert = {
-        title: "Ce service n'est pas un projet officiel",
-        description:
-            "Il a pour but d'implémenter le DSFR à des fins de test par un particulier et n'est aucunement associé aux équipes digitales des entités de l'État français.",
-    };
-}
+export class App {}

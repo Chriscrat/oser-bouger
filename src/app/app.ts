23dc98753a937e7bc4033f65b9aab019b643b2d1
Christophe Barreto <dev-cba@pm.me>
2026-07-24 16:49:57 +0200

    feat(ui): add ThemeToggle component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,10 @@ import { Component } from "@angular/core";
 import { Header } from "./layout/components/header/header";
 import { Footer } from "./layout/components/footer/footer";
 import { RouterOutlet } from "@angular/router";
-
+import { ThemeToggle } from "./ui/theme-toggle/components/theme-toggle";
 @Component({
     selector: "app-root",
-    imports: [Header, Footer, RouterOutlet],
+    imports: [Header, Footer, RouterOutlet, ThemeToggle],
     templateUrl: "./app.html",
     styleUrl: "./app.scss",
 })

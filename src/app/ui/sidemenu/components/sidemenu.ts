05f3606405c5ab8103086c13bca8cdd502885d81
Christophe Barreto <dev-cba@pm.me>
2026-07-27 23:29:57 +0200

    refactor(ui): make Sidemenu template generic

--- src/app/ui/sidemenu/components/sidemenu.ts
+++ src/app/ui/sidemenu/components/sidemenu.ts
@@ -1,17 +1,12 @@
-import { Component, HostListener, inject, input } from "@angular/core";
-import { EventFilters2 } from "../../../features/events/models/event-filters";
-import { EventFilter } from "../../../features/events/components/event-filter/components/event-filter";
+import { Component, HostListener, inject } from "@angular/core";
 import { SidemenuService } from "../services/sidemenu.service";
 
 @Component({
     selector: "app-sidemenu",
     templateUrl: "./sidemenu.html",
-    imports: [EventFilter],
     styleUrl: "./sidemenu.scss",
 })
 export class Sidemenu {
-    filters = input.required<EventFilters2[]>();
-
     private sidemenuService = inject(SidemenuService);
     isOpen = this.sidemenuService.isOpen;
 

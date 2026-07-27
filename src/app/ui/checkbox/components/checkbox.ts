10a2ddaa71a0b2c5613e9c40f10337e60e66be9f
Christophe Barreto <dev-cba@pm.me>
2026-07-27 23:27:28 +0200

    refactor(ui): add output function "change" on Checkbox component

--- src/app/ui/checkbox/components/checkbox.ts
+++ src/app/ui/checkbox/components/checkbox.ts
@@ -1,4 +1,4 @@
-import { Component, input } from "@angular/core";
+import { Component, input, output } from "@angular/core";
 
 @Component({
     selector: "app-checkbox",
@@ -6,4 +6,9 @@ import { Component, input } from "@angular/core";
 })
 export class Checkbox {
     title = input.required<string>();
+    checkedChange = output<boolean>();
+
+    onChange(event: Event): void {
+        this.checkedChange.emit((event.target as HTMLInputElement).checked);
+    }
 }

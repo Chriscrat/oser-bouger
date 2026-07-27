f8d3c9ed75e9ed7d9ce190272bb3e70b21a8bbe8
Christophe Barreto <dev-cba@pm.me>
2026-07-27 23:31:48 +0200

    refactor(ui): remove dead code on home-page component

--- src/app/features/home/pages/home-page.ts
+++ src/app/features/home/pages/home-page.ts
@@ -2,7 +2,6 @@ import { Component } from "@angular/core";
 import { Alert } from "../../../ui/alert/components/alert";
 import { Hero } from "../../home/components/hero/hero";
 import { EventList } from "../../events/components/event-list/event-list";
-import { EventFilters2 } from "../../events/models/event-filters";
 import { Sidemenu } from "../../../ui/sidemenu/components/sidemenu";
 @Component({
     selector: "app-home-page",
@@ -17,34 +16,4 @@ export class HomePage {
         description:
             "Il a pour but d'implémenter le DSFR à des fins de test par un particulier et n'est aucunement associé aux équipes digitales des entités de l'État français.",
     };
-
-    filters: EventFilters2[] = [
-        {
-            name: "Test 1",
-            filters: [
-                {
-                    text: "val1",
-                    value: "val1",
-                },
-            ],
-        },
-        {
-            name: "Test 2",
-            filters: [
-                {
-                    text: "val1",
-                    value: "val1",
-                },
-            ],
-        },
-        {
-            name: "Test 3",
-            filters: [
-                {
-                    text: "val1",
-                    value: "val1",
-                },
-            ],
-        },
-    ];
 }

0fbb075e0b5f1b557e8855642358b8ec926f05ed
Christophe Barreto <dev-cba@pm.me>
2026-07-27 23:47:33 +0200

    feat(filters): implement event filters using facets
    All facets are retrieved from the API. There are used
    to build filters then saved on the Event store.
    By clicking on checkbox from the sidemenu, the event list
    is filtered.

--- src/app/features/events/components/event-filter/components/event-filter.ts
+++ src/app/features/events/components/event-filter/components/event-filter.ts
@@ -1,12 +1,14 @@
-import { Component, input } from "@angular/core";
-import { EventFilters2 } from "../../../models/event-filters";
+import { Component, input, inject, Input } from "@angular/core";
+import { EventFilters, FilterName } from "../../../models/event-filters";
 import { Checkbox } from "../../../../../ui/checkbox/components/checkbox";
-
+import { EventsStore } from "../../../services/events.store";
 @Component({
     selector: "app-event-filter",
     imports: [Checkbox],
     templateUrl: "./event-filter.html",
 })
 export class EventFilter {
-    filter = input.required<EventFilters2>();
+    filter = input.required<EventFilters>();
+    store = inject(EventsStore);
+    @Input() onUpdate!: (filter: FilterName, value: string) => void;
 }

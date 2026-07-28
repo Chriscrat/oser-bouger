3fac65fede6bdef49beaec00de4b26372c315843
Christophe Barreto <dev-cba@pm.me>
2026-07-28 14:01:21 +0200

    fix(build): adjust the budget of the initial bundle for the DSFR CSS

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -14,7 +14,8 @@ export default {
                 "events",
                 "card",
                 "map",
-                "filters"
+                "filters",
+                "build",
             ],
         ],
     },

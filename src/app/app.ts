1fe1136d96cc6db497f9b1bbe910671b0b1328e7
Christophe Barreto <dev-cba@pm.me>
2026-09-16 23:57:27 +0200

    feat(filters): retrieve main event category list from api

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { Header } from "./layout/components/header/header";
 import { Footer } from "./layout/components/footer/footer";
 import { RouterOutlet } from "@angular/router";
 import { ThemeToggle } from "./ui/theme-toggle/components/theme-toggle";
 
+import { EventsStore } from "./features/events/services/events.store";
 @Component({
     selector: "app-root",
     imports: [Header, Footer, RouterOutlet, ThemeToggle],
     templateUrl: "./app.html",
 })
-export class App {}
+export class App implements OnInit {
+    store = inject(EventsStore);
+    ngOnInit(): void {
+        void this.store.fetchCategoryList();
+    }
+}
